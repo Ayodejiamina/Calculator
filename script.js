ae2c3ed7863f07523e055e7a29e2dd27451ae1d7
Ayodeji Amina Ayomide <ayodejiamina@gmail.com>
2022-08-01 09:32:55 +0100

    calculator project

---
+++ script.js
@@ -0,0 +1,70 @@
+function number7(){
+    document.getElementById("result").value += '7';
+    
+}
+
+function number8(){
+    document.getElementById("result").value += '8'
+}
+
+function number9(){
+    document.getElementById("result").value += '9'
+}
+
+function cleardisplay(){
+    document.getElementById("result").value = ''
+}
+
+// function operator1(){
+//     document.getElementById("result").value += <i class="fas fa-power-off text" style="color: red;"></i>
+// }
+
+function number4(){
+    document.getElementById("result").value += '4'
+}
+
+function number5(){
+    document.getElementById("result").value += '5'
+}
+
+function number6(){
+    document.getElementById("result").value += '6'
+}
+
+function number1(){
+    document.getElementById("result").value += '1'
+}
+function number2(){
+    document.getElementById("result").value += '2'
+}
+function number3(){
+    document.getElementById("result").value += '3'
+}
+function operator2(){
+    document.getElementById("result").value += '*'
+}
+function operator3(){
+    document.getElementById("result").value += '('
+}
+function operator4(){
+    document.getElementById("result").value += '_'
+}
+function operator5(){
+    document.getElementById("result").value += ')'
+}
+function operator6(){
+    document.getElementById("result").value += '.'
+}
+function operator7(){
+    document.getElementById("result").value += '/'
+}
+function operator8(){
+    document.getElementById("result").value += '+'
+}
+function operator9(){
+    document.getElementById("result").value = eval(document.getElementById("result").value)
+}
+function number0(){
+    document.getElementById("result").value +='0'
+}
+
